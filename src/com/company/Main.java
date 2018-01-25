abab0a151d85cd4345f50bf6903b14dbaa78dcf0
Victoria West <victoriawest811@gmail.com>
2018-01-25 16:31:35 -0500

    Finished Project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,49 @@
+
 package com.company;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        /*
+        Fizzbuzz - Take user input of 1 - 100
+        if it is a multiple of 3 output "Fizz"
+        if it is a multiple of 5 output "Buzz"
+        of it is a multiple of both output "FizzBuzz"
+        if it is none of these output the user number
+
+         */
+
+
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number 1 - 100:");
+        int userNum = input.nextInt();
+
+         if (userNum % 5 == 0 && userNum % 3 == 0 )
+            System.out.println("FizzBuzz.");
+
+        else if ( userNum % 3 == 0)
+            System.out.println("Fizz.");
+
+         else if (userNum % 5 == 0)
+            System.out.println("Buzz.");
+
+         else {
+            System.out.println(userNum);
+        }
+
+
+
+
+
+
+
+
     }
 }
+;
